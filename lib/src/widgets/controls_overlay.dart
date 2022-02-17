46916a279bbc64c6cb01b740e07fa011d3d9adc3
no-do-not-track-me <no-do-not-track-me@gmail.com>
2022-02-17 12:49:27 +0530

    added volume control

--- lib/src/widgets/controls_overlay.dart
+++ lib/src/widgets/controls_overlay.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-class ControlsOverlay extends StatelessWidget {
-  const ControlsOverlay({Key? key, required this.controller})
-      : super(key: key);
+class ControlsOverlay extends StatefulWidget {
+  const ControlsOverlay({Key? key, required this.controller}) : super(key: key);
 
+  final VideoPlayerController controller;
+
+  @override
+  _ControlsOverlayState createState() => _ControlsOverlayState();
+}
+
+class _ControlsOverlayState extends State<ControlsOverlay> {
   static const _examplePlaybackRates = [
     0.5,
     1.0,
@@ -12,7 +18,8 @@ class ControlsOverlay extends StatelessWidget {
     2.0,
   ];
 
-  final VideoPlayerController controller;
+  var volume = 1.0;
+  var playing = true;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +28,7 @@ class ControlsOverlay extends StatelessWidget {
         AnimatedSwitcher(
           duration: const Duration(milliseconds: 50),
           reverseDuration: const Duration(milliseconds: 200),
-          child: controller.value.isPlaying
+          child: playing
               ? const SizedBox.shrink()
               : Container(
                   color: Colors.black26,
@@ -37,37 +44,57 @@ class ControlsOverlay extends StatelessWidget {
         ),
         GestureDetector(
           onTap: () {
-            controller.value.isPlaying ? controller.pause() : controller.play();
+            setState(() {
+              widget.controller.value.isPlaying
+                  ? widget.controller.pause()
+                  : widget.controller.play();
+              playing = widget.controller.value.isPlaying;
+            });
           },
         ),
-        Align(
-          alignment: Alignment.topRight,
-          child: PopupMenuButton<double>(
-            initialValue: controller.value.playbackSpeed,
-            tooltip: 'Playback speed',
-            onSelected: (speed) {
-              controller.setPlaybackSpeed(speed);
-            },
-            itemBuilder: (context) {
-              return [
-                for (final speed in _examplePlaybackRates)
-                  PopupMenuItem(
-                    value: speed,
-                    child: Text('${speed}x'),
-                  )
-              ];
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                // Using less vertical padding as the text is also longer
-                // horizontally, so it feels like it would need more spacing
-                // horizontally (matching the aspect ratio of the video).
-                vertical: 12,
-                horizontal: 16,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Spacer(),
+            SizedBox(
+              height: 30,
+              child: Slider(
+                value: volume,
+                onChanged: (value) {
+                  setState(() {
+                    volume = value;
+                    widget.controller.setVolume(value);
+                  });
+                },
               ),
-              child: Text('${controller.value.playbackSpeed}x'),
             ),
-          ),
+            PopupMenuButton<double>(
+              initialValue: widget.controller.value.playbackSpeed,
+              tooltip: 'Playback speed',
+              onSelected: (speed) {
+                widget.controller.setPlaybackSpeed(speed);
+              },
+              itemBuilder: (context) {
+                return [
+                  for (final speed in _examplePlaybackRates)
+                    PopupMenuItem(
+                      value: speed,
+                      child: Text('${speed}x'),
+                    )
+                ];
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  // Using less vertical padding as the text is also longer
+                  // horizontally, so it feels like it would need more spacing
+                  // horizontally (matching the aspect ratio of the video).
+                  vertical: 12,
+                  horizontal: 16,
+                ),
+                child: Text('${widget.controller.value.playbackSpeed}x'),
+              ),
+            )
+          ],
         ),
       ],
     );
