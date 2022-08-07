929420a3d3f8c8614430208a8e07d84d73062add
no-do-not-track-me <no-do-not-track-me@gmail.com>
2022-08-08 00:27:18 +0530

    post to hive updated.

--- lib/src/screens/my_account/my_account_screen.dart
+++ lib/src/screens/my_account/my_account_screen.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-import 'dart:developer';
-
 import 'package:acela/src/bloc/server.dart';
-import 'package:acela/src/models/login/login_bridge_response.dart';
 import 'package:acela/src/models/user_stream/hive_user_stream.dart';
 import 'package:acela/src/models/video_details_model/video_details.dart';
 import 'package:acela/src/screens/my_account/account_settings/account_settings_screen.dart';
@@ -10,7 +6,6 @@ import 'package:acela/src/utils/communicator.dart';
 import 'package:acela/src/widgets/custom_circle_avatar.dart';
 import 'package:acela/src/widgets/loading_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:provider/provider.dart';
 
@@ -24,8 +19,8 @@ class MyAccountScreen extends StatefulWidget {
 class _MyAccountScreenState extends State<MyAccountScreen> {
   Future<List<VideoDetails>>? loadVideos;
   Future<void>? loadOperations;
-  var isLoading = false;
-  var loadingText = '';
+  // var isLoading = false;
+  // var loadingText = '';
 
   void logout() async {
     // Create storage
@@ -36,47 +31,47 @@ class _MyAccountScreenState extends State<MyAccountScreen> {
     Navigator.of(context).pop();
   }
 
-  void loadVideoInfo(HiveUserData user, String videoId) async {
-    setState(() {
-      isLoading = true;
-      loadingText = 'Getting video data to post on Hive';
-    });
-    try {
-      var result = await Communicator().loadOperations(user, videoId);
-      var utf8data = utf8.encode(result);
-      final base64Str = base64.encode(utf8data);
-      setState(() {
-        loadingText = 'Publishing on Hive';
-      });
-      var platform = MethodChannel('com.example.acela/auth');
-      final String response = await platform.invokeMethod('postVideo', {
-        'data': base64Str,
-        'postingKey': user.postingKey,
-      });
-      var bridgeResponse = LoginBridgeResponse.fromJsonString(response);
-      if (bridgeResponse.valid == true) {
-        setState(() {
-          loadingText = 'Marking video as published';
-        });
-        await Communicator().updatePublishState(user, videoId);
-        setState(() {
-          isLoading = false;
-          loadVideos = Communicator().loadVideos(user);
-        });
-      } else {
-        showError('Error occurred: ${bridgeResponse.error}');
-      }
-      log('Result from android platform is \n$response');
-      setState(() {
-        isLoading = false;
-      });
-    } catch (e) {
-      setState(() {
-        isLoading = false;
-      });
-      showError('Error occurred - ${e.toString()}');
-    }
-  }
+  // void loadVideoInfo(HiveUserData user, String videoId) async {
+  //   setState(() {
+  //     isLoading = true;
+  //     loadingText = 'Getting video data to post on Hive';
+  //   });
+  //   try {
+  //     var result = await Communicator().loadOperations(user, videoId);
+  //     var utf8data = utf8.encode(result);
+  //     final base64Str = base64.encode(utf8data);
+  //     setState(() {
+  //       loadingText = 'Publishing on Hive';
+  //     });
+  //     var platform = MethodChannel('com.example.acela/auth');
+  //     final String response = await platform.invokeMethod('postVideo', {
+  //       'data': base64Str,
+  //       'postingKey': user.postingKey,
+  //     });
+  //     var bridgeResponse = LoginBridgeResponse.fromJsonString(response);
+  //     if (bridgeResponse.valid == true) {
+  //       setState(() {
+  //         loadingText = 'Marking video as published';
+  //       });
+  //       await Communicator().updatePublishState(user, videoId);
+  //       setState(() {
+  //         isLoading = false;
+  //         loadVideos = Communicator().loadVideos(user);
+  //       });
+  //     } else {
+  //       showError('Error occurred: ${bridgeResponse.error}');
+  //     }
+  //     log('Result from android platform is \n$response');
+  //     setState(() {
+  //       isLoading = false;
+  //     });
+  //   } catch (e) {
+  //     setState(() {
+  //       isLoading = false;
+  //     });
+  //     showError('Error occurred - ${e.toString()}');
+  //   }
+  // }
 
   void showError(String string) {
     var snackBar = SnackBar(content: Text('Error: $string'));
@@ -96,14 +91,21 @@ class _MyAccountScreenState extends State<MyAccountScreen> {
           Text(username),
         ],
       ),
+      bottom: const TabBar(
+        tabs: [
+          Tab(icon: Icon(Icons.hourglass_top)),
+          Tab(icon: Icon(Icons.rocket_launch)),
+          Tab(icon: Icon(Icons.check)),
+        ],
+      ),
       actions: [
         IconButton(
           onPressed: () {
-            var screen = AccountSettingsScreen();
+            var screen = const AccountSettingsScreen();
             var route = MaterialPageRoute(builder: (c) => screen);
             Navigator.of(context).push(route);
           },
-          icon: Icon(Icons.settings),
+          icon: const Icon(Icons.settings),
         )
       ],
     );
@@ -111,18 +113,18 @@ class _MyAccountScreenState extends State<MyAccountScreen> {
 
   Widget _trailingActionOnVideoListItem(VideoDetails item, HiveUserData user) {
     return item.status == 'published'
-        ? Icon(Icons.check, color: Colors.green)
+        ? const Icon(Icons.check, color: Colors.green)
         : item.status == 'publish_manual'
             ? IconButton(
                 onPressed: () {
-                  loadVideoInfo(user, item.id);
+                  // loadVideoInfo(user, item.id);
                 },
-                icon: Icon(
+                icon: const Icon(
                   Icons.rocket_launch,
                   color: Colors.green,
                 ),
               )
-            : Icon(
+            : const Icon(
                 Icons.hourglass_top,
                 color: Colors.blue,
               );
@@ -142,13 +144,40 @@ class _MyAccountScreenState extends State<MyAccountScreen> {
     );
   }
 
-  Widget _videosList(List<VideoDetails> items, HiveUserData user) {
+  Widget _listViewForItems(List<VideoDetails> items, HiveUserData user) {
+    if (items.isEmpty) {
+      return const Center(
+        child: Text('No Items found.'),
+      );
+    }
     return ListView.separated(
       itemBuilder: (context, index) {
         return _videoListItem(items[index], user);
       },
       separatorBuilder: (context, index) => const Divider(),
       itemCount: items.length,
+    );
+  }
+
+  Widget _videosList(List<VideoDetails> items, HiveUserData user) {
+    var published = items.where((item) => item.status == 'published').toList();
+    var ready = items.where((item) => item.status == 'publish_manual').toList();
+    var process = items
+        .where((item) =>
+            item.status != 'published' && item.status != 'publish_manual')
+        .toList();
+    return TabBarView(
+      children: [
+        SafeArea(
+          child: _listViewForItems(process, user),
+        ),
+        SafeArea(
+          child: _listViewForItems(ready, user),
+        ),
+        SafeArea(
+          child: _listViewForItems(published, user),
+        ),
+      ],
     );
   }
 
@@ -180,19 +209,14 @@ class _MyAccountScreenState extends State<MyAccountScreen> {
       });
     }
     var username = user?.username ?? 'Unknown';
-    return Scaffold(
-      appBar: _appBar(username),
-      body: Container(
-        child: user == null
-            ? const Center(child: Text('Nothing'))
-            : isLoading
-                ? Center(
-                    child: LoadingScreen(
-                      title: 'Publishing Your Video',
-                      subtitle: loadingText,
-                    ),
-                  )
-                : _videoFuture(user),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: _appBar(username),
+        body: Container(
+            child: user == null
+                ? const Center(child: Text('Nothing'))
+                : _videoFuture(user)),
       ),
     );
   }
