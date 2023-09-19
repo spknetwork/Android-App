532c7f94fa49e848840fe036eeb913b361160c74
no-do-not-track-me <no-do-not-track-me@gmail.com>
2023-09-19 19:54:30 +0530

    reformatting

--- lib/src/screens/podcast/local_podcast_episode.dart
+++ lib/src/screens/podcast/local_podcast_episode.dart
@@ -12,81 +12,79 @@ class LocalPodcastEpisode extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-  length: 2,
-  child: Scaffold(
-    appBar: AppBar(
-      title: Text('Podcast Episodes'),
-      bottom: TabBar(
-        tabs: [
-
-          Tab(text: 'Offline Episode'),
-          Tab(text: 'Liked Episode'),
-        ],
-      ),
-    ),
-    body: TabBarView(
-      children: [
-         episodePlayerView(podcastEpisodes(isOffline : true), context),
-          episodePlayerView(podcastEpisodes(isOffline :false), context)
-      ],
-    ),
-  ),
-);
-
-  }
-
-  Widget episodePlayerView(List<PodcastEpisode> items,BuildContext context){
-    if(items.isEmpty)
-          return Center(child: Text("Offline Podcast Episode is Empty"));
-          else return ListView.separated(
-              itemBuilder: (c, index) {
-                PodcastEpisode item = items[index];
-                return ListTile(
-                  onTap: () {
-                    var screen = Scaffold(
-                      appBar: AppBar(
-        title: ListTile(
-          leading: Image.network(
-            item.image ?? '',
-            width: 40,
-            height: 40,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Podcast Episodes'),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Offline Episode'),
+              Tab(text: 'Liked Episode'),
+            ],
           ),
-          title: Text(item.title ?? 'No Title'),
+        ),
+        body: TabBarView(
+          children: [
+            episodePlayerView(podcastEpisodes(isOffline: true), context),
+            episodePlayerView(podcastEpisodes(isOffline: false), context)
+          ],
         ),
       ),
-                      body: PodcastEpisodePlayer(
-                        episodeIndex: index,
-                          data: appData, podcastEpisodes: items),
-                    );
-                    var route = MaterialPageRoute(builder: (c) => screen);
-                    Navigator.of(context).push(route);
-                  },
-                  leading: Container(
-                    height: 30,
-                    width: 30,
-                    decoration: BoxDecoration(
-                        color: Colors.grey,
-                        image: DecorationImage(
-                            image: NetworkImage(
-                              item.image ?? "",
-                            ),
-                            fit: BoxFit.cover)),
+    );
+  }
+
+  Widget episodePlayerView(List<PodcastEpisode> items, BuildContext context) {
+    if (items.isEmpty)
+      return Center(child: Text("Offline Podcast Episode is Empty"));
+    else
+      return ListView.separated(
+        itemBuilder: (c, index) {
+          PodcastEpisode item = items[index];
+          return ListTile(
+            onTap: () {
+              var screen = Scaffold(
+                appBar: AppBar(
+                  title: ListTile(
+                    leading: Image.network(
+                      item.image ?? '',
+                      width: 40,
+                      height: 40,
+                    ),
+                    title: Text(item.title ?? 'No Title'),
                   ),
-                  title: Text(
-                        item.title ?? '',
-                        maxLines: 2,
-                        style: Theme.of(context).textTheme.titleSmall,
+                ),
+                body: PodcastEpisodePlayer(
+                    episodeIndex: index, data: appData, podcastEpisodes: items),
+              );
+              var route = MaterialPageRoute(builder: (c) => screen);
+              Navigator.of(context).push(route);
+            },
+            leading: Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                  color: Colors.grey,
+                  image: DecorationImage(
+                      image: NetworkImage(
+                        item.image ?? "",
                       ),
-                );
-              },
-              separatorBuilder: (c, i) => const Divider(height: 0),
-              itemCount: items.length,
-            );
+                      fit: BoxFit.cover)),
+            ),
+            title: Text(
+              item.title ?? '',
+              maxLines: 2,
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+          );
+        },
+        separatorBuilder: (c, i) => const Divider(height: 0),
+        itemCount: items.length,
+      );
   }
 
   List<PodcastEpisode> podcastEpisodes({required bool isOffline}) {
     final box = GetStorage();
-    final String key =isOffline ? 'offline_podcast' : "liked_podcast_episode";
+    final String key = isOffline ? 'offline_podcast' : "liked_podcast_episode";
     if (box.read(key) != null) {
       List json = box.read(key);
       List<PodcastEpisode> items =
